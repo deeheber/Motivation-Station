ce4ca1b8e1bbc37bbc475e46922a91976dc65da1
Danielle H <daheber@gmail.com>
2016-05-19 08:11:25 -0700

    quote writes to the page

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('it works');
+var quoteContainer = document.getElementById('quoteContainer');
+
+var generateQuote = function(){
+  //generate random number between 0 and quote.length
+  var randNum = Math.floor(Math.random() * quote.length);
+  console.log(randNum);
+
+  //check to make sure the random number isn't the same as the prior random number
+
+  //write quote[random number] to the DOM
+  quoteContainer.innerHTML = quote[randNum];
+};
+
+//run script when page loads and when clicking the link for more
+generateQuote();
+
+// for(var i=0; i<quote.length; i++){
+//   console.log(quote[i]);
+// }
---
+++ quotes.js
@@ -0,0 +1,7 @@
+var quote = [
+  "quote 1",
+  "quote 2",
+  "quote 3",
+  "quote 4",
+  "quote 5"
+];
