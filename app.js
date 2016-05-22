6d83c5d7f97dde6e0280a741fbfbd30cfe70e5a8
Danielle H <daheber@gmail.com>
2016-05-22 12:38:37 -0700

    check to verify the same quote isn't picked in a row

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var quoteContainer = document.getElementById('quoteContainer');
+var priorQuote = -1; //store the last array index
 
 var generateQuote = function(){
   //generate random number between 0 and quote.length
-  var randNum = Math.floor(Math.random() * quote.length);
-  console.log(randNum);
+  var newQuote = Math.floor(Math.random() * quote.length);
 
   //check to make sure the random number isn't the same as the prior random number
+  while(newQuote === priorQuote){
+    randNum = Math.floor(Math.random() * quote.length);
+  }
 
-  //write quote[random number] to the DOM
-  quoteContainer.innerHTML = quote[randNum];
+  //write quote to the DOM
+  quoteContainer.innerHTML = quote[newQuote];
+  priorQuote = newQuote;
 };
 
 //run script when page loads and when clicking the link for more
 generateQuote();
-
-// for(var i=0; i<quote.length; i++){
-//   console.log(quote[i]);
-// }
