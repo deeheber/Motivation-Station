886bf0b179377bd1c328f94b87d10fb97667dfc1
Danielle H <daheber@gmail.com>
2016-05-22 13:03:01 -0700

    event listeners

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var quoteContainer = document.getElementById('quoteContainer');
+var moreLink = document.getElementById('more');
 var priorQuote = -1; //store the last array index
 
-var generateQuote = function(){
+var generateQuote = function(event){
+  event.preventDefault();
   //generate random number between 0 and quote.length
   var newQuote = Math.floor(Math.random() * quote.length);
 
+  //TODO: refactor to prevent infinite loops
   //check to make sure the random number isn't the same as the prior random number
-  while(newQuote === priorQuote){
-    randNum = Math.floor(Math.random() * quote.length);
-  }
+  // while(newQuote === priorQuote){
+  //   randNum = Math.floor(Math.random() * quote.length);
+  // }
 
   //write quote to the DOM
   quoteContainer.innerHTML = quote[newQuote];
@@ -16,4 +19,5 @@ var generateQuote = function(){
 };
 
 //run script when page loads and when clicking the link for more
-generateQuote();
+window.addEventListener('load', generateQuote);
+moreLink.addEventListener('click', generateQuote);
