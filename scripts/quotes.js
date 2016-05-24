b9738ee1be3def1769c533b9e2adc1fe829d11b9
Danielle H <daheber@gmail.com>
2016-05-23 19:05:19 -0700

    first 10 quotes added

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -1,7 +1,12 @@
 var quote = [
-  "quote 1",
-  "quote 2",
-  "quote 3",
-  "quote 4",
-  "quote 5"
+  "If you want something you never had, you have to do something you've never done.",
+  "If you want your dream job, create it.",
+  "The doors will open for those bold enough to knock.",
+  "Only sleep in if you make money in your sleep.",
+  "Be humble, be hungry, and always be the hardest worker in the room.",
+  "You don't get paid for what you know, but for what you do with what you know.",
+  "If you don't go after what you want, you'll never have it.",
+  "No one ever achieves greatness by playing it safe.",
+  "Learn to build your empire with the bricks thrown at you.",
+  "Overthinking kills too many dreams."
 ];
